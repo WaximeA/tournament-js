0a507c9fad0108c51df6b36aa4c615aaf439b9b1
Maxime Amore <maxime.marco.amore@gmail.com>
2019-02-06 12:16:45 +0100

    merging dev

--- modules/Tournament.js
+++ modules/Tournament.js
@@ -1,6 +1,6 @@
 import Player from './Player.js'
 import Options from './Options.js'
-import Bracket from './Match.js'
+import Bracket from './Bracket.js'
 import Form  from './Form.js'
 
 export default class Tournament {
@@ -49,22 +49,25 @@ export default class Tournament {
     }
   }
 
-    display() {
-        for (Match in this.Matches) {
-            Match.display(this.options);
-        }
+  display() {
+    for (bracket in this.brackets) {
+      bracket.display(this.options)
     }
+  }
 
-    createMatch() {
-        //on doit calculer le nb de Match necessaire
-        this.nbMatches = this.calcNbMatch();
-        //on push autant de Match que necessaire
-        for (let i = 0; i < this.nbMatches; i++){
-            this.Matchs.push(new Match(/*firstPlayer, secondPlayer, options*/));
-        }
+  createBracket() {
+    //on doit calculer le nb de bracket necessaire
+    this.nbBrackets = this.calcNbBracket()
+    //on push autant de bracket que necessaire
+    for (let i = 0; i < this.nbBrackets; i++) {
+      this.brackets.push(new Bracket(/*firstPlayer, secondPlayer, options*/))
     }
+  }
 
-    calcNbMatch() {
-        //on calcule le nb de Match en fonction du nb de joueurs
-    }
-};
+  calcNbBracket() {
+    //on calcule le nb de bracket en fonction du nb de joueurs
+  }
+
+
+
+}
