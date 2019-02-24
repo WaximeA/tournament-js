838099b8ac81dafb868b25c80d89d4e44f36da33
waxime <aveline.maxime@gmail.com>
2019-02-24 16:04:35 +0100

    bracket: WIP manage bracket

--- modules/Tournament.js
+++ modules/Tournament.js
@@ -1,28 +1,25 @@
-import Player from './Player.js'
-import Options from './Options.js'
-import Match from './Match.js'
-import Form  from './Form.js'
+import Player from './Player.js';
+import Options from './Options.js';
+import Match from './Match.js';
+import Form from './Form.js';
 
 const knownBrackets = [2, 4, 8, 16, 32, 64];
-
-// @todo fonction à ajouter un helper avec des fonctions globales.
-function isOdd(num) { return num % 2;}
 
 export default class Tournament {
   constructor(players, options) {
 
-    this.players = []
+    this.players = [];
 
     this.options = {
-      score : false,
-      type : "classique"
-    }
+      score: false,
+      type: 'classique',
+    };
 
     this.matches = [];
-    this.nbBrackets = 0
+    this.nbBrackets = 0;
 
-    this.form = new Form()
-    this.form.initForm()
+    this.form = new Form();
+    this.form.initForm();
   }
 
   /**
@@ -30,7 +27,8 @@ export default class Tournament {
    * @param {*} player : id, nom du joueur et s'il est vainqueur
    */
   addPlayer(player) {
-    this.players.push(new Player(player.id, player.name, player.isPlayerWinner))
+    this.players.push(
+        new Player(player.id, player.name, player.isPlayerWinner));
   }
 
   /**
@@ -38,38 +36,38 @@ export default class Tournament {
    * à partir des inputs du formulaire
    * @param {e} target html
    */
-  buildTournament(e){
+  buildTournament(e) {
 
     // On sait que le premier input est le type de tournoi
-    this.options.type = e.target[0].value
-    this.options.score = e.target[1].checked
+    this.options.type = e.target[0].value;
+    this.options.score = e.target[1].checked;
 
     // Pour chaque input, on ajoute le joueur en question
-    for (let index = 2; index < e.target.length-1; index++) {
+    for (let index = 2; index < e.target.length - 1; index++) {
       let isPlayerWinner = false;
-      if (isOdd(e.target.length-1) && index === e.target.length-2) {
+      if (isOdd(e.target.length - 1) && index === e.target.length - 2) {
         isPlayerWinner = true;
       }
 
       this.addPlayer({
-        id : index-1,
-        name : e.target[index].value,
-        isPlayerWinner : isPlayerWinner
-      })
+        id: index - 1,
+        name: e.target[index].value,
+        isPlayerWinner: isPlayerWinner,
+      });
     }
     this.createMatches();
   }
 
   display() {
     for (bracket in this.brackets) {
-      bracket.display(this.options)
+      bracket.display(this.options);
     }
   }
 
   createMatches() {
     let nbRound = 1;
     let nbJoueur = this.players.length;
-    let closestKnownBracket = knownBrackets.find(function (elem) {
+    let closestKnownBracket = knownBrackets.find(function(elem) {
       return elem >= nbJoueur;
     });
     let matches = [];
@@ -94,7 +92,8 @@ export default class Tournament {
           let joueur1 = null;
           let joueur2 = null;
           //si on a plus de joueurs, on arrete la boucle
-          if (typeof this.players[t] === 'undefined' && typeof this.players[t + 1] === 'undefined') {
+          if (typeof this.players[t] === 'undefined' &&
+              typeof this.players[t + 1] === 'undefined') {
             break;
           }
           //construction des matchs dans un round
@@ -108,9 +107,9 @@ export default class Tournament {
           }
 
           let match = new Match(idMatch, noMatch, [
-              joueur1,
-              joueur2
-            ], null, null, matchHasWinner);
+            joueur1,
+            joueur2,
+          ], null, null, matchHasWinner);
           //on met dans le tableau du round en cours un match identifié par son numéro de match
           matches[nbRound][noMatch] = match;
           idMatch++;
@@ -128,20 +127,21 @@ export default class Tournament {
           previousM2 = previousRound[(noMatch * 2) - 1];
           //récupération des joueurs gagnants des matchs précédents
           if (previousM1 && previousM1.hasWinner === true) {
-            let joueurWinnerPM = previousM1.joueurs.find(function (j) {
+            let joueurWinnerPM = previousM1.joueurs.find(function(j) {
               //a changer
               return j.isWinner;
             });
             JoueursMatch.push(joueurWinnerPM);
           }
           if (previousM2 && previousM2.hasWinner === true) {
-            let joueurWinnerPM = previousM2.joueurs.find(function (j) {
+            let joueurWinnerPM = previousM2.joueurs.find(function(j) {
               //a changer
               return j.isWinner;
             });
             JoueursMatch.push(joueurWinnerPM);
           }
-          let match = new Match(idMatch, noMatch, JoueursMatch, previousM1, previousM2, false);
+          let match = new Match(idMatch, noMatch, JoueursMatch, previousM1,
+              previousM2, false);
           matches[nbRound][noMatch] = match;
           noMatch++;
           idMatch++;
@@ -153,11 +153,9 @@ export default class Tournament {
   }
 
   createBracket(tournament) {
+    console.log(tournament);
 
-    Promise
-    .resolve()
-    .then(() => {
-      console.log(tournament);
+    Promise.resolve().then(() => {
       let root = document.getElementById('root');
       let bracket = tournament.matches;
       let bracketContainer = document.createElement('div');
@@ -167,60 +165,97 @@ export default class Tournament {
       for (let index = 1; index < bracket.length; index++) {
         let round = bracket[index];
         let roundContainer = document.createElement('div');
-        roundContainer.className = 'round round-'+index;
+        roundContainer.className = 'round round-' + index;
 
         // Chaque match
         for (let index2 = 1; index2 < round.length; index2++) {
           let matchup = round[index2];
           let matchupContainer = document.createElement('div');
-          matchupContainer.className = 'matchup matchup-'+ matchup.id;
+          matchupContainer.className = 'matchup matchup-' + matchup.id;
 
           // Chaque joueur
           if (matchup.joueurs.length > 0) {
-            for (let index3 = 0; index3 < matchup.joueurs.length; index3++) {
-              let playerContainer = document.createElement('div');
-              let previousPlayer = matchup.joueurs[index3-1];
-              let player = matchup.joueurs[index3];
-              if (player){
-                playerContainer.className = 'player player-' + player.id;
-                playerContainer.textContent = player.name;
-              } else {
-                // Déclaration du joueur gagnant si aucun opposant
-                previousPlayer.isWinner = true;
-                playerContainer.className = 'player player-undefined';
-                playerContainer.textContent = 'no opponent';
+            // Test seulement pour le premier round
+            if (index === 1) {
+              for (let index3 = 0; index3 < 2; index3++) {
+                let playerContainer = document.createElement('div');
+                let player = matchup.joueurs[index3];
+                if (player) {
+                  // Affichage du joueur
+                  playerContainer.className = 'player player-' + player.id;
+                  playerContainer.textContent = player.name;
+                } else {
+                  // Affichage d'un message comme quoi il n'y a pas d'opposant
+                  playerContainer.className = 'player player-undefined';
+                  playerContainer.textContent = 'No opponent.';
+                }
+                matchupContainer.appendChild(playerContainer);
               }
-              matchupContainer.appendChild(playerContainer);
-            }
-          } else {
-            // Definition des match en attente
-            for (let index4 = 1; index4 < 3 ; index4++) {
-              let pendingPlayerContainer = document.createElement('div');
-              pendingPlayerContainer.className = 'player pending-player';
-              pendingPlayerContainer.textContent = 'Pending player.';
-              matchupContainer.appendChild(pendingPlayerContainer);
+            } else {
+              if (isPreviousMatcheshHasWinner(matchup)){
+                // Ici il faudrait récupérer les vainqueur de chaque match précédent
+              }
             }
           }
-          roundContainer.appendChild(matchupContainer)
+          // A décommenter pour voir la génération de l'abre OK avec un nombre de participant pair.
+          // Ca ne fonctionne pas avec un nombre de participant impaire
+          // else {
+          //   // Definition des match en attente
+          //   if (!matchup.joueurs.length > 0) {
+          //   for (let index4 = 1; index4 < 3; index4++) {
+          //     let pendingPlayerContainer = document.createElement('div');
+          //     pendingPlayerContainer.className = 'player pending-player';
+          //     pendingPlayerContainer.textContent = 'Pending player.';
+          //     matchupContainer.appendChild(pendingPlayerContainer);
+          // }}}
+
+          // Ajout du match au round courrant
+          roundContainer.appendChild(matchupContainer);
         }
+        // Ajout du round au bracket
         bracketContainer.appendChild(roundContainer);
-       }
+      }
 
-       // Winner container
-      let winnerRound = document.createElement('div');
-      winnerRound.className = 'round winner';
-      let winnerMatchup = document.createElement('div');
-      winnerMatchup.className = 'matchup winner';
-      let winnerPlayer = document.createElement('div');
-      winnerPlayer.textContent = 'No winner yet';
-      winnerPlayer.className = 'player winner';
-      winnerMatchup.appendChild(winnerPlayer);
-      winnerRound.appendChild(winnerMatchup);
-      bracketContainer.appendChild(winnerRound);
+      // Ajout du noeud du vainqueur du brakcet
+      getWinnerContainer(bracketContainer);
 
-       // root.append(bracketContainer);
-       root.replaceWith(bracketContainer);
-       console.log(bracketContainer);
-    })
+      // Aout du brakket au container ou remplacement du container
+      // root.append(bracketContainer);
+      root.replaceWith(bracketContainer);
+      console.log(bracketContainer);
+    });
   }
+}
+
+// TODO FONCTIONS CI-DESSOUS A METTRE DANS UN HELPER POUR QUE CA SOIT PLUS PROPRE
+function isOdd(num) { return num % 2;}
+
+function getWinnerContainer(bracketContainer) {
+  // Création du round, matchup et player gagnant
+  let winnerRound = document.createElement('div');
+  let winnerMatchup = document.createElement('div');
+  let winnerPlayer = document.createElement('div');
+  // Attribution des classes
+  winnerRound.className = 'round winner';
+  winnerMatchup.className = 'matchup winner';
+  winnerPlayer.className = 'player winner';
+  winnerPlayer.textContent = 'No winner yet';
+  // Ajout des divs au bracket
+  winnerMatchup.appendChild(winnerPlayer);
+  winnerRound.appendChild(winnerMatchup);
+  bracketContainer.appendChild(winnerRound);
+}
+
+function isPreviousMatcheshHasWinner(matchup) {
+  let previousM1 = matchup.previousMatch1;
+  let previousM2 = matchup.previousMatch2;
+  let isPreviousMatcheshHasWinner = false;
+
+  if (previousM1) {
+    isPreviousMatcheshHasWinner = previousM1.hasWinner;
+  } else if (previousM2) {
+    isPreviousMatcheshHasWinner = previousM2.hasWinner;
+  }
+
+  return isPreviousMatcheshHasWinner;
 }
